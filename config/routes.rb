973b8297a1ac07fd67f95e068ff22fe0c56c33c2
nanarow <woranan.sholee@gmail.com>
2024-12-04 17:01:25 +0700

    root

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "food_items#recommended"
 end
